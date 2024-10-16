7fafda17a19ffad35a64a9dc71da70ffc3310ba6
Pontus Curtsson <pontuscurtsson@gmail.com>
2024-10-16 11:17:58 +0200

    modified commands

--- mergeMain.py
+++ mergeMain.py
@@ -7,7 +7,7 @@ def runCommand(command):
     except Exception as e:
         return f"Error: {e}"
 
-# Switches from develop branch to release branch
-runCommand("git checkout release")
-# Merges current branch (release) into specified branch (main)
-runCommand("git merge origin/main")
+# Switches from develop branch to main branch
+runCommand("git checkout origin/main")
+# Merges given branch (release) into specified branch (release)
+runCommand("git merge origin/release")
