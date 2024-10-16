00a75749781333c64dbd506e5bd417fae65fdda6
Pontus Curtsson <pontuscurtsson@gmail.com>
2024-10-16 11:59:30 +0200

    Commands

--- mergeDown.py
+++ mergeDown.py
@@ -9,7 +9,7 @@ def runCommand(command):
 
 # Switches to relase branch and pulls changes from main
 runCommand("git checkout release")
-runCommand("git merge main")
+runCommand("git merge main -m'Merges main into release branch'")
 # Switches to develop branch and pulls changes from main
 runCommand("git merge develop")
-runCommand("git merge main")
+runCommand("git merge main -m'Merges main into develop branch'")
