77e662cdb319c9f2a04b5a9c83c34e73ef1cf2e4
Pontus Curtsson <pontuscurtsson@gmail.com>
2024-10-16 12:17:17 +0200

    Modified

--- createReleaseBranch.py
+++ createReleaseBranch.py
@@ -3,11 +3,12 @@ import subprocess
 def runCommand(command):
     try:
         result = subprocess.run(command)
+        print(result)
         return result.stdout
     except Exception as e:
         return f"Error: {e}"
     
 # Creates a new branch "release" based on develop
-print(runCommand("git branch release"))
+runCommand("git branch release")
 # Pushes changes from develop to release
-print(runCommand("git push origin release"))
+runCommand("git push origin release")
